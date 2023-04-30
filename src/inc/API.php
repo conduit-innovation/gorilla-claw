79e5a8a47c9161bba7a645aaf0030b27c18851c7
Tom Lawton <tom.lawton@cndu.it>
2023-04-30 03:42:17 +0000

    doc: API

--- src/inc/API.php
+++ src/inc/API.php
@@ -2,20 +2,56 @@
 
 namespace MonkeyHook;
 
-function find_actions(string | array $hook_name, mixed $callback = false) {
+/**
+ * find_actions
+ *
+ * @param  mixed $hook_name
+ * @param  mixed $callback
+ * @return HookCollection
+ */
+
+function find_actions(string | array $hook_name, mixed $callback = false): HookCollection {
     return find_filters($hook_name, $callback);
 }
 
-function find_filters(string | array $hook_name, mixed $callback = false) {
+/**
+ * find_filters
+ *
+ * @param  mixed $hook_name
+ * @param  mixed $callback
+ * @return HookCollection
+ */
+
+function find_filters(string | array $hook_name, mixed $callback = false): HookCollection {
     global $wp_filter;
     return (new Query($wp_filter))->find($hook_name, $callback);
 }
 
-function add_actions(string | array $hook_name, $callback, $priority, $accepted_args) {
-    return add_filters($hook_name, $callback, $priority, $accepted_args);
+/**
+ * add_actions
+ *
+ * @param  mixed $hook_name
+ * @param  mixed $callback
+ * @param  mixed $priority
+ * @param  mixed $accepted_args
+ * @return void
+ */
+
+function add_actions(string | array $hook_name, $callback, $priority, $accepted_args): void {
+    add_filters($hook_name, $callback, $priority, $accepted_args);
 }
 
-function add_filters(string | array $hook_names, $callback, $priority = 10, $accepted_args = 1) {
+/**
+ * add_filters
+ *
+ * @param  mixed $hook_names
+ * @param  mixed $callback
+ * @param  mixed $priority
+ * @param  mixed $accepted_args
+ * @return void
+ */
+
+function add_filters(string | array $hook_names, $callback, $priority = 10, $accepted_args = 1): void {
     if(is_string($hook_names)) {
         $hook_names = explode(" ", $hook_names);
     }
